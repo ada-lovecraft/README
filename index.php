6231b4e5309ce10e8748494016b6af417da25cb6
Jeremy <jeremy@codevinsky.com>
2013-02-01 16:51:00 -0600

    Testing

--- index.php
+++ index.php
@@ -6,7 +6,24 @@ $f3->set('UI','views/');
 
 $f3->route('GET /', 
 	function($f3) {
+		$url=parse_url('mysql://b7171039c55598:b2e8bb66@us-cdbr-east-03.cleardb.com/heroku_87f7c241b70c126?reconnect=true');
+		var_dump($url);
+		
+	    $server = $url["host"];
+    	$username = $url["user"];
+    	$password = $url["pass"];
+    	$db = substr($url["path"],1);
+
+    mysql_connect($server, $username, $password);
+            
+		$db=new DB\SQL(
+		    'mysql:host='.$server.';port=3306;dbname='.$db,
+		    $username,
+			$password
+		);
+		
 		echo View::instance()->render('index.htm');
+		
 	}
 );
 
