fd054b431b6f84c645ff6a041efe5118a33d76d5
Jeremy <jeremy@codevinsky.com>
2013-02-01 17:09:41 -0600

    System Messages

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 $f3=require('lib/base.php');
 
-$f3->set('UI','views/');
+$f3->set('UI','ui/');
 
 $f3->route('GET /', 
 	function($f3) {
@@ -20,8 +20,70 @@ $f3->route('GET /',
 			$password
 		);
 		
-		echo View::instance()->render('index.htm');
+		echo View::instance()->render('views/index.htm');
 		
+	}
+);
+
+$f3->route('GET /system',
+	function($f3) {
+		$classes=array(
+			'Base'=>
+				array(
+					'hash',
+					'json',
+					'session'
+				),
+			'Cache'=>
+				array(
+					'apc',
+					'memcache',
+					'wincache',
+					'xcache'
+				),
+			'DB\SQL'=>
+				array(
+					'pdo',
+					'pdo_dblib',
+					'pdo_mssql',
+					'pdo_mysql',
+					'pdo_odbc',
+					'pdo_pgsql',
+					'pdo_sqlite',
+					'pdo_sqlsrv'
+				),
+			'DB\Jig'=>
+				array('json'),
+			'DB\Mongo'=>
+				array(
+					'json',
+					'mongo'
+				),
+			'Auth'=>
+				array('ldap','pdo'),
+			'Image'=>
+				array('gd'),
+			'Lexicon'=>
+				array('iconv'),
+			'SMTP'=>
+				array('openssl'),
+			'Web'=>
+				array('curl','openssl','simplexml'),
+			'Web\Geo'=>
+				array('geoip','json'),
+			'Web\OpenID'=>
+				array('json','simplexml'),
+			'Web\Pingback'=>
+				array('dom','xmlrpc')
+		);
+		$f3->set('classes',$classes);
+		echo View::instance()->render('welcome.htm');
+	}
+);
+
+$f3->route('GET /userref',
+	function() {
+		echo View::instance()->render('userref.htm');
 	}
 );
 
